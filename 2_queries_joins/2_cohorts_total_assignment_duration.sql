cd124ff402ac94d7620d404c71f5547dc14f2498
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-02-01 19:04:20 -0500

    made some modifications to the query

--- 2_queries_joins/2_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -3,3 +3,10 @@ FROM assignment_submissions
 JOIN students ON student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name = 'FEB12';
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date = NULL
+GROUP BY students.name
+HAVING COUNT(assignment_submissions.*) < 100;
