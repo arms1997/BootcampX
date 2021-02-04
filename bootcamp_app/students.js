9ae83d920d84a90b9ce63b3b5f723859f5de53de
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-02-03 20:01:34 -0500

    parametrize queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
+const values = [`%${process.argv[2]}%`, process.argv[3]]
 
 pool.query(`
   SELECT students.id, students.name, cohorts.name as cohort_name
   FROM students
   JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3]};
-`)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const values = [`${process.argv[2]}`]
+
 pool.query(`
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, COUNT(assistance_requests.*) as total_assistances
   FROM teachers
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON assistance_requests.student_id = students.id
   JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${process.argv[2]}'
+  WHERE cohorts.name = $1
   GROUP BY teachers.name, cohorts.name
   ORDER BY total_assistances DESC;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(data => {
     console.log(`${data.cohort}: ${data.teacher}`);
